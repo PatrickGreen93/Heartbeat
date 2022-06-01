b8f74b37b02a5c0accd7f6ffbdc8b5d82551ffc0
PatrickGreen93 <pag016@latech.edu>
2022-06-01 01:22:41 -0500

    Initial version.

---
+++ R/heartbeat.R
@@ -0,0 +1,126 @@
+#' Conduct Heartbeat Analysis
+#'
+#' @param data A data.frame.
+#' @param type Type of output, default is "counts" which provides data.frame of counts, "standard" standardizes scores of original data.frame, and "votes" converts the standardization score into up-vote/down-vote/neutral.
+#' @param threshold This denotes the range of "Neutral", or conversey, what deviation score above/below the base-rate is necessary to count as an up/down-vote. Default = 1.
+#' @param props Default is false. If props = TRUE, will convert counts of each vote into proportions. Will only change output for type = "counts".
+#' @param controversial Default is true. If controversial = FALSE, function will not provide additional controversy score for each item.
+#' @return Number of up-votes, down-votes, and neutral for each variable in data.frame \code{data}.
+#' @examples
+#' \dontrun{heartbeat(data, type = "counts", threshold = 0.5, props = TRUE, controversial = FALSE)}
+#' @import dplyr
+#' @import tidyr
+#' @export
+heartbeat <- function(data, type = c("counts", "standard", "votes"), threshold = 1, props = FALSE, controversial = TRUE){
+
+
+  if(missing(data)){
+    stop("Please provide a data.frame containing numeric variables.")
+  }
+
+  if(missing(data) == FALSE && is.data.frame(data) == FALSE){
+    data <- as.data.frame(data)
+  }
+
+  if(is.data.frame(data) == FALSE){
+    stop("Must pass a data.frame or object that can be converted to data.frame.")
+  }
+
+  data <- as.data.frame(sapply(data,as.numeric))
+
+  if(missing(data) == TRUE && is.data.frame(data) == TRUE && all(sapply(data,is.numeric)) == FALSE){
+    stop("All variables must contain numeric values.")
+  }
+
+  if(missing(data) == TRUE && is.data.frame(data) == TRUE && all(sapply(data,is.numeric)) == FALSE && all(is.na(data)) == TRUE){
+    warning("Dataset contained NA Values which will have been dropped prior to calculation. This could be a result of coercing data to Numeric.")
+  }
+
+  type <- match.arg(type)
+
+  if(is.numeric(threshold) == FALSE){
+    stop("Threshold must be a numeric value.")
+  }
+
+  . <- individual.sd <- individual.mean <- Item <- Var1 <- Freq <- NULL
+    # Not necessary, likely not advisable to do this either. Just doing it for R CMD check...
+
+  likert.items <- colnames(data)
+
+  Standard <- data %>%
+    mutate(
+      individual.sd =
+        sqrt(rowSums((select(data, likert.items) -
+                        rowMeans(select(data, all_of(likert.items)),na.rm = T))^2,
+                     na.rm = T) /
+               (rowSums(!is.na(select(data, all_of(likert.items)))) - 1)),
+      individual.mean =
+        rowMeans(select(data, all_of(likert.items)),na.rm = T)) %>%
+    mutate_at(likert.items, function(x){
+      case_when(.$individual.sd == 0 ~ 0,
+                .$individual.sd != 0 ~
+                  (x - .$individual.mean) / (.$individual.sd)
+      )}) %>%
+    subset(., select = -c(individual.sd, individual.mean))
+
+  Votes <- Standard %>%
+    mutate_at(likert.items, function(x){
+      case_when(x > threshold ~ 1,
+                x < -1*threshold ~ -1,
+                x < threshold & x > -1*threshold ~ 0)
+    }
+    )
+
+
+  Counts <- Votes %>%
+    lapply(table) %>%
+    lapply(as.data.frame) %>%
+    Map(cbind, Item = names(Votes),.) %>%
+    bind_rows() %>%
+    group_by(Item) %>%
+    spread(Var1, Freq) %>%
+    .[match(likert.items, .$Item),] %>%
+    as.data.frame()
+
+  Counts[is.na(Counts)] <- 0
+  colnames(Counts)[2:4] <- c("Down_Vote", "Neutral", "Up_Vote")
+
+  Cont <- Counts %>%
+    mutate(
+      Controversial = ((Counts$Up_Vote + Counts$Down_Vote) - abs(Counts$Up_Vote - Counts$Down_Vote))/(Counts$Neutral)
+    ) %>%
+    as.data.frame()
+
+  Counts_Proportion <- Counts %>%
+    mutate_if(is.numeric, ~ . / rowSums(select(Counts, where(is.numeric))))
+
+  Cont_Proportion <- cbind(Counts_Proportion, "Controversial" = Cont[,5])
+
+
+
+
+
+
+  if(type == "standard"){
+    Standard
+  }
+  else if(type == "votes"){
+    Votes
+  }
+  else if(type == "counts"){
+    if(controversial == TRUE){
+      if (props == TRUE){
+        Cont_Proportion
+      }else{
+        Cont
+      }
+    }else{
+      if (props == TRUE){
+        Counts_Proportion
+      }else{
+        Counts
+      }
+    }
+  }
+
+}
---
+++ README.md
@@ -0,0 +1,61 @@
+
+<!-- README.md is generated from README.Rmd. Please edit that file -->
+
+# Heartbeat
+
+<!-- badges: start -->
+<!-- badges: end -->
+
+The goal of the Heartbeat package is to provide users with a fast and
+easy method of conducting Heartbeat Analysis on Numeric Likert data with
+a single function. Heartbeat Analysis itself is a simple method of
+gauging sentiment of respondents at the item level by using within-group
+methods to highlight when an individual’s response is significantly
+higher or lower than their own typical baseline.
+
+Additionally, there are several arguments that can be used within this
+package which are covered more in-depth in the Introduction.rmd file.
+
+## Installation
+
+Installation of the package can be done by copying the line of code
+below.
+
+``` r
+devtools::install_github("PatrickGreen93/Heartbeat")
+```
+
+## Example
+
+Below is a basic example of how the code works and a reproducible
+dataset.
+
+``` r
+library(Heartbeat)
+# For the purpose of this vignette, I will create a reproducible data.frame
+set.seed(43)
+n <- 30
+y <- 100
+example <- data.frame()
+for (i in 1:n){
+  x <- data.frame(sample(1:5, y, replace = TRUE, prob = c(0.1, 0.2, 0.4, 0.2, 0.1)))
+  colnames(x) <- paste0("Q",i)
+  example[1:y,i] <- x
+}
+example[1:5,1:5]
+#>   Q1 Q2 Q3 Q4 Q5
+#> 1  2  4  3  4  3
+#> 2  1  4  2  1  3
+#> 3  3  4  2  4  4
+#> 4  4  2  2  4  3
+#> 5  3  2  3  3  3
+
+example <- heartbeat(example, type = "counts", threshold = 1, props = FALSE, controversial = TRUE)
+example[1:5,]
+#>   Item Down_Vote Neutral Up_Vote Controversial
+#> 1   Q1        14      75      11     0.2933333
+#> 2   Q2        18      68      14     0.4117647
+#> 3   Q3        22      59      19     0.6440678
+#> 4   Q4        16      68      16     0.4705882
+#> 5   Q5        18      70      12     0.3428571
+```
